90a1c84fbcea4db32f83cb4cd8c4cb572672896b
SeungAhSon <48306744+SeungAhSon@users.noreply.github.com>
2022-06-09 18:56:09 +0900

    [Silver III] Title: N과 M (3), Time: 1480 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,14 @@
+N, M = list(map(int,input().split()))
+ans = []
+
+def dfs():
+    if len(ans)==M:
+        print(' '.join(map(str,ans)))
+        return
+    
+    for i in range(1,N+1):
+        ans.append(i)
+        dfs()
+        ans.pop()
+
+dfs()
