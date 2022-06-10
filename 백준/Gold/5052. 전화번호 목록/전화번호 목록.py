8f6ed0056495fa3b2c3920ac267e1de1a7319262
SeungAhSon <48306744+SeungAhSon@users.noreply.github.com>
2022-06-10 21:30:41 +0900

    [Gold IV] Title: 전화번호 목록, Time: 224 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -1,16 +1,17 @@
-def check():
-    for i in range(len(phone)-1):
-        if phone[i]==phone[i+1][:len(phone[i])]:
-            print("NO")
-            return
-    print("YES")
-
+import sys
 T = int(input())
 
 for _ in range(T):
     N = int(input())
     phone = []
-    for i in range(N):
-        phone.append(input().strip())
+    for _ in range(N):
+        phone.append(sys.stdin.readline().strip())
     phone.sort()
-    check()
+    
+    flag = 1
+    for i in range(len(phone)-1):
+        if phone[i]==phone[i+1][:len(phone[i])]:
+            flag = 0
+        
+    if flag == 1: print("YES")
+    else : print("NO")
