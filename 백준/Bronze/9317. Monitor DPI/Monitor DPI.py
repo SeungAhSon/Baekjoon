a32de6dc3b93b73cf27c9835312084988c9647f1
SeungAhSon <48306744+SeungAhSon@users.noreply.github.com>
2022-06-13 21:54:17 +0900

    [Bronze III] Title: Monitor DPI, Time: 268 ms, Memory: 32952 KB -BaekjoonHub

--- 백준/Bronze/9317. Monitor DPI/Monitor DPI.py
+++ 백준/Bronze/9317. Monitor DPI/Monitor DPI.py
@@ -3,4 +3,4 @@ while(True):
     A,B,C = map(float,input().split())
     if(A+B+C==0) : break
     print("Horizontal DPI: %.2f"%(B*(math.sqrt(337)/16/A)))
-    print("Vertical DPI: %.2f"%(C/(9*A/math.sqrt(337))))
+    print("Vertical DPI: %.2f"%(C*math.sqrt(337)/9/A))
