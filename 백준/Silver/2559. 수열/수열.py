1648111aebeb19e8e021bd35351cce624d1a5f49
SeungAhSon <48306744+SeungAhSon@users.noreply.github.com>
2022-06-16 12:59:12 +0900

    [Silver III] Title: 수열, Time: 112 ms, Memory: 39572 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.py
@@ -0,0 +1,10 @@
+import sys
+N,K = map(int, sys.stdin.readline().split())
+
+temp = list(map(int,sys.stdin.readline().split()))
+ans = [sum(temp[:K])]
+
+
+for i in range(1, N-K+1):
+    ans.append(ans[-1]+temp[i+K-1]-temp[i-1])
+print(max(ans))
