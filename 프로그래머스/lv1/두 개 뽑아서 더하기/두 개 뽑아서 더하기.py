ed375f86ab3fbacb9df0e9d813add6e66c54b9bf
SeungAhSon <48306744+SeungAhSon@users.noreply.github.com>
2022-06-19 22:30:20 +0900

    Delete 프로그래머스/lv1/두 개 뽑아서 더하기 directory

--- 프로그래머스/lv1/두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++
@@ -1,6 +0,0 @@
-from itertools import combinations
-def solution(numbers):
-    answer = list(set([sum([i,j]) for i,j in combinations(numbers,2)]))
-    answer = list(set(answer)) # 중복제거
-    answer.sort()
-    return answer
