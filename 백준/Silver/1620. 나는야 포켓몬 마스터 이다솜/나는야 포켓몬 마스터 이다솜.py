d0ce8a993a15bac5e052902b80ed16edb8379639
SeungAhSon <48306744+SeungAhSon@users.noreply.github.com>
2022-06-28 13:53:29 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 272 ms, Memory: 52864 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,3 +1,22 @@
+'''
+#첫 시도
+#시간 초과
+
+import sys
+N,M = map(int, sys.stdin.readline().split())
+
+pokemon = ['0']
+for _ in range(N):
+    temp = sys.stdin.readline().lower()[:-1]
+    if temp not in pokemon : pokemon.append(temp)
+
+for i in range(M):
+    temp = sys.stdin.readline()[:-1]
+    if temp.isdigit(): print(pokemon[int(temp)][0].upper()+pokemon[int(temp)][1:])
+    else : print(pokemon.index(temp.lower()))
+'''
+
+
 import sys
 N,M = map(int, sys.stdin.readline().split())
 
