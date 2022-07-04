b893ad19bc0eab245496c621e95e26c4edfd2662
SeungAhSon <48306744+SeungAhSon@users.noreply.github.com>
2022-07-04 22:23:08 +0900

    [Unrated] Title: Rick, Time: 104 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Unrated/15639. Rick/Rick.py
@@ -0,0 +1,5 @@
+Rick =  ['Give you up', 'Let you down', 'Run around and desert you', 'Make you cry', 'Say goodbye', 'Tell a lie and hurt you'];
+temp = input()
+
+if temp in Rick : print("NO")
+else : print("YES")
