fc39607be401dffbde7bec5b2c9dcb009f7b6e13
SeungAhSon <48306744+SeungAhSon@users.noreply.github.com>
2022-07-11 22:29:07 +0900

    [Silver II] Title: INFP 두람 , Time: 96 ms, Memory: 42240 KB -BaekjoonHub

--- 백준/Silver/25186. INFP 두람 /INFP 두람 .py
+++ 백준/Silver/25186. INFP 두람 /INFP 두람 .py
@@ -2,6 +2,5 @@ N = int(input())
 cloth = list(map(int, input().split()))
 if N == 1 and cloth[0]==1: print("Happy")
 else :
-    cloth.sort()
-    if cloth[-1]*2>sum(cloth) : print("Unhappy")
+    if max(cloth)*2>sum(cloth) : print("Unhappy")
     else : print("Happy")
