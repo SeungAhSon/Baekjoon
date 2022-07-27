136e9602ac375cd98bdd6641b44cc4bb5e0e62ce
SeungAhSon <48306744+SeungAhSon@users.noreply.github.com>
2022-07-27 21:47:39 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,13 @@
+def solution(num):
+    if(num==1) : return 0
+
+    ans = 0
+    for i in range(500):
+        num = num//2 if num%2==0 else num*3+1
+        ans += 1 
+        if num==1 : break
+        if(num!=1 and ans>=500):
+            ans = -1
+            break
+        
+    return ans
