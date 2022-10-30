dfcca8f658fad2f3344356ac7ad80565aae304c9
SeungAhSon <48306744+SeungAhSon@users.noreply.github.com>
2022-10-30 14:25:27 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,6 +1,4 @@
 def solution(num_list):
-    count = 0
-    for i in num_list:
-        if i%2==0 : count+=1
-    
-    return [count, len(num_list)-count]
+    answer = [0,0]
+    for i in num_list: answer[i%2]+=1
+    return answer
