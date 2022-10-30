c92d211da889298ecb6bcf4c929e28a21ddc79a0
SeungAhSon <48306744+SeungAhSon@users.noreply.github.com>
2022-10-30 14:34:21 +0900

    [level 0] Title: OX퀴즈, Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.py
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.py
@@ -2,16 +2,7 @@ def solution(quiz):
     answer = []
     
     for i in quiz:
-        formula, z = i.split("=")
-        formula = formula.split()
-        
-        if formula[1] == "+":
-            formula = int(int(formula[0]) + int(formula[2]))
-            
-        elif formula[1] == "-":
-            formula = int(int(formula[0]) - int(formula[2]))
-        
-        if formula == int(z) : answer.append("O")
-        else : answer.append("X")
+        a, b = i.split("=")
+        answer.append("O" if eval(a)==int(b) else "X")
 
     return answer
