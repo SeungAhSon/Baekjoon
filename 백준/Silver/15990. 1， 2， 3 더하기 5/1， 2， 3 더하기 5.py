0f3b5203845f82c129e2694125631f06e14b7415
SeungAhSon <48306744+SeungAhSon@users.noreply.github.com>
2023-01-31 21:27:56 +0900

    [Silver II] Title: 1, 2, 3 더하기 5, Time: 184 ms, Memory: 51492 KB -BaekjoonHub

---
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+dp = [[0]*4 for _ in range(100001)]
+dp[1] = [0,1,0,0]
+dp[2] = [0,0,1,0]
+dp[3] = [0,1,1,1]
+ 
+for i in range(4, 100001):
+  dp[i][1] = (dp[i-1][2] + dp[i-1][3])
+  dp[i][2] = (dp[i-2][1] + dp[i-2][3])
+  dp[i][3] = (dp[i-3][1] + dp[i-3][2])
+ 
+  dp[i][1] %= 1000000009
+  dp[i][2] %= 1000000009
+  dp[i][3] %= 1000000009
+ 
+T = int(input())
+for i in range(T):
+  n = int(input())
+  print(sum(dp[n])% 1000000009)
