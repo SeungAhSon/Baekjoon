335791ce35ddf997297543231af39747431c2333
SeungAh Son <48306744+SeungAhSon@users.noreply.github.com>
2023-02-14 13:58:20 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 4.46 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.c
+++ 프로그래머스/lv0/120829. 각도기/각도기.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 
 int solution(int angle) {
-    if(angle<90){return 1;}
-    else if(angle==90){return 2;}
-    else if(angle<180){return 3;}
-    else{return 4;}
+    if(angle<90) return 1;
+    else if(angle==90) return 2;
+    else if(angle<180) return 3;
+    else return 4;
 }
