5e9ee596aed9610306349944da5a75baf4b27d7c
SeungAh Son <48306744+SeungAhSon@users.noreply.github.com>
2023-03-17 15:30:20 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.py
+++ 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.py
@@ -1,5 +1,2 @@
 def solution(my_str, n):
-    if len(my_str)%n>0:
-        return [my_str[i*n:(i+1)*n] for i in range(0, len(my_str)//n)]+[my_str[-(len(my_str)%n):len(my_str)]]
-    else:
-        return [my_str[i*n:(i+1)*n] for i in range(0, len(my_str)//n)]
+    return [my_str[i: i + n] for i in range(0, len(my_str), n)]
