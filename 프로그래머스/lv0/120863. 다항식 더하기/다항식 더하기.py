03877918b936f05205b65107890effc3d2b41514
SeungAh Son <48306744+SeungAhSon@users.noreply.github.com>
2023-03-19 00:55:28 +0900

    [level 0] Title: 다항식 더하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.py
+++ 프로그래머스/lv0/120863. 다항식 더하기/다항식 더하기.py
@@ -1,19 +1,11 @@
+from collections import defaultdict
 def solution(polynomial):
+    poly_dic = defaultdict(int)
     poly = list(map(str,polynomial.split(" + ")))
-    a = []
-    b = []
-    for i in poly:
-        if i[-1]=='x':
-            a.append(1 if len(i)==1 else int(i[0:-1]))
-        else : b.append(int(i))
-        
-    a,b=sum(a),sum(b)
-    if a==0:
-        if b==0: return ''
-        else : return str(b)
-    elif a==1: 
-        if b==0: return 'x'
-        else : return 'x + '+str(b)
-    else:
-        if b==0: return str(a)+'x'
-        else: return str(a)+'x + '+str(b)
+    for i in range(len(poly)):
+        if (poly[i][-1] == "x"):
+            poly_dic['x'] += int(poly[i].replace('x',''))
+        else:
+            poly_dic['-'] += int(poly[i])
+    answer = poly_dic['x'] + 'x' + poly_dic['-']
+    return answer
