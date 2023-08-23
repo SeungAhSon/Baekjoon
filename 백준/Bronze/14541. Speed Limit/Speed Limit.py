fbde1b9c42bee0e3b4615bb6ed78367a897a7f1d
SeungAh Son <gongsoonyee@gmail.com>
2023-08-23 11:07:58 +0900

    [Bronze III] Title: Speed Limit, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/14541. Speed Limit/Speed Limit.py
@@ -0,0 +1,12 @@
+import sys
+
+while True:
+    N = int(sys.stdin.readline())
+    if N == -1: break
+    last_time = 0
+    distance = 0
+    for _ in range(N):
+        s, t = map(int, sys.stdin.readline().split())
+        distance += s*(t-last_time)
+        last_time = t
+    print(f"{distance} miles")
