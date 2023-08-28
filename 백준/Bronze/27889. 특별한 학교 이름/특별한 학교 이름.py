cd69f0c8cb98343e96e5addbba8cd240da039bdd
SeungAh Son <gongsoonyee@gmail.com>
2023-08-28 13:31:47 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,8 @@
+import sys
+
+N = sys.stdin.readline().rstrip()
+
+if N=="NLCS": print("North London Collegiate School")
+elif N=="BHA": print("Branksome Hall Asia")
+elif N=="KIS": print("Korea International School")
+elif N=="SJA": print("St. Johnsbury Academy")
