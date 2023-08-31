9d9d0732543d78a7f96f00e818ff911b1dae9a94
SeungAh Son <gongsoonyee@gmail.com>
2023-08-31 11:23:59 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,7 @@
+import sys
+
+N = sys.stdin.readline().rstrip()
+if N=="SONGDO" : print("HIGHSCHOOL")
+elif N=="CODE" : print("MASTER")
+elif N=="2023" : print("0611")
+elif N=="ALGORITHM" : print("CONTEST")
