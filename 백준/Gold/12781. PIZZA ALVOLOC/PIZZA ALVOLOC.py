d72097898540ad368df15d0c05314700fea2e21b
SeungAhSon <gongsoonyee@gmail.com>
2024-02-23 01:54:10 +0900

    Update PIZZA ALVOLOC.py

--- 백준/Gold/12781. PIZZA ALVOLOC/PIZZA ALVOLOC.py
+++ 백준/Gold/12781. PIZZA ALVOLOC/PIZZA ALVOLOC.py
@@ -1,6 +1,7 @@
 import sys
 
-#네 조각으로 나눠지는지 알아야하므로 ccw를 이용해서
+#네 조각으로 나눠지는지 알아야하므로 ccw를 이용한다.
+#17386번과 유사한 문제
 x1, y1, x2, y2, x3, y3, x4, y4 = map(int, sys.stdin.readline().split())
 dot1, dot2, dot3, dot4 = [x1, y1], [x2, y2], [x3, y3], [x4, y4]
 
