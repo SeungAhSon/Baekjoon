0952fb7b7ff5e332dc05d07cb58475af29e32fc3
SeungAhSon <gongsoonyee@gmail.com>
2024-03-01 21:48:34 +0900

    [Bronze I] Title: A/B - 2, Time: 136 ms, Memory: 109108 KB, Score: 2000 point -BaekjoonHub

--- 백준/Bronze/15792. A／B － 2/A／B － 2.py
+++ 백준/Bronze/15792. A／B － 2/A／B － 2.py
@@ -1,2 +1,10 @@
 A,B = map(int, input().split())
-print(A/B)
+print(A//B, end = '')
+
+if A%B:
+    print('.', end='')
+    i = 0
+    while A%B and i < 1000: 
+        A = A%B*10 
+        i += 1
+        print(A//B, end = '')
