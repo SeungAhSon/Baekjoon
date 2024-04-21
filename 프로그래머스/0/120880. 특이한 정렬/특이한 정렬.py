e0a96b8b37d3734f4da03056ca1c30c196cdfac2
SeungAhSon <gongsoonyee@gmail.com>
2024-04-22 00:18:53 +0900

    [level 0] Title: 특이한 정렬, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.py
@@ -1,5 +1,4 @@
 def solution(numlist, n):
-    new_numlist = [(i, abs(i-n)) for i in numlist]
-    new_numlist.sort(key=lambda x:(x[1],-x[0]))
+    numlist.sort(key=lambda x:(abs(x-n),-x))
 
-    return [i[0] for i in new_numlist]
+    return numlist
