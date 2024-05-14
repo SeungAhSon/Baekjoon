1f1211418e8741e0a4a72fc3fa2941da9dd47584
SeungAhSon <gongsoonyee@gmail.com>
2024-05-15 02:04:23 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -2,5 +2,6 @@ def solution(num, total):
     # 등차수열의 합 : n*(start+end)/2
     # ==> total = num*(s+s+num-1)/2
     # ==> s = (total/num*2-num+1)/2
+    
     s = int((total/num*2-num+1)/2)
     return [i for i in range(s, s+num, 1)]
